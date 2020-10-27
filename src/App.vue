f2fba448dfbe5ee249551e0956156847f77e1c08
nurakmaljalil91 <nurakmaljalil91@gmail.com>
2020-10-27 11:32:43 +0800

    add sections

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,58 @@
 <template>
   <v-app>
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list-item color="primary">
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            BursaNews Web API
+          </v-list-item-title>
+          <v-list-item-subtitle> subtext </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense nav color="grey lighten-1" dark>
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
     <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center"></div>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon
+    ></v-app-bar>
 
-      <v-spacer></v-spacer>
-    </v-app-bar>
+    <v-main>
+      <v-row>
+        <v-col cols="6"><SummarySection /></v-col>
 
-    <v-main> </v-main>
+        <v-col cols="6"><ExampleSection /></v-col>
+      </v-row>
+    </v-main>
   </v-app>
 </template>
 
 <script>
+import ExampleSection from "@/components/ExampleSection";
+import SummarySection from "@/components/SummarySection";
 export default {
   name: "App",
+  components: {
+    ExampleSection,
+    SummarySection,
+  },
+  data() {
+    return {
+      drawer: null,
+      items: [
+        { title: "GET /api/BursaNews" },
+        { title: "GET /api/BursaNews/{newsid}" },
+        { title: "About" },
+      ],
+      right: null,
+    };
+  },
 };
 </script>
---
+++ src/components/ExampleSection.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <v-container fluid>
+      <v-sheet flat color="grey darken-4" dark outlined>
+        <h1>Example Section</h1>
+      </v-sheet>
+    </v-container>
+  </div>
+</template>
