a932f1317bab68fd91485f449d6bddc2ea89be17
nurakmaljalil91 <nurakmaljalil91@gmail.com>
2020-10-29 19:09:17 +0800

    update  data json

--- src/components/Overview.vue
+++ src/components/Overview.vue
@@ -4,7 +4,18 @@
       <h1>Overview</h1>
       <v-divider></v-divider>
       <h2>1. Introduction</h2>
-      
+      <p>{{overview.introduction}}</p>
     </v-container>
   </div>
 </template>
+
+<script>
+import dataJson from "@/assets/data.json";
+export default {
+  data() {
+    return {
+      overview : dataJson.overviews
+    };
+  },
+};
+</script>
